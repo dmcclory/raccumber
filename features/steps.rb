b62cc2a75719437e4bb526a3e3714cb7b0e96b4e
Dan <danmcclory@gmail.com>
2013-07-13 16:14:15 -0400

    add a raccumber 'bin' file which loads step definitions from .rb files in features

---
+++ features/steps.rb
@@ -0,0 +1,15 @@
+Given /that I have an object named (.*)/ do |name|
+  name
+end
+
+And /I have a second object named (.*)/ do |name|
+  name
+end
+
+When /I have a combine them/ do |name|
+  name
+end
+
+Then /I have a different object named (.*)/ do |name|
+  name
+end
--- lib/raccumber/step_definitions.rb
+++ lib/raccumber/step_definitions.rb
@@ -16,18 +16,3 @@ end
 
 include StepDefinitions
 
-Given /that I have an object named (.*)/ do |name|
-  name
-end
-
-And /I have a second object named (.*)/ do |name|
-  name
-end
-
-When /I have a combine them/ do |name|
-  name
-end
-
-Then /I have a different object named (.*)/ do |name|
-  name
-end
