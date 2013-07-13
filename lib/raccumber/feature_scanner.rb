bcffbe785d4fcce005b7d23dee2492c29afe6f73
Dan <danmcclory@gmail.com>
2013-07-13 17:18:41 -0400

    FeatureScanner approximates the gherkin syntax.

---
+++ lib/raccumber/feature_scanner.rb
@@ -0,0 +1,30 @@
+class FeatureScanner
+
+  def initialize
+  end
+
+  def tokenize text
+    tokens = []
+
+    while text != ""
+      case text
+      when /\AFeature: /
+        tokens << [:FEAT_KEYWORD, $&]
+        text.slice!(0, $&.size)
+      when /\AScenario: /
+        tokens << [:SCEN_KEYWORD, $&]
+        text.slice!(0, $&.size)
+      when /\AGiven: |\AWhen: |\AAnd: |\AThen: /
+        tokens << [:STEP_KEYWORD, $&]
+        text.slice!(0, $&.size)
+      when /\A\n/ 
+        text.slice!(0, $&.size)
+      when /\A[^\n]*/ 
+        tokens << [:TEXT, $&]
+        text.slice!(0, $&.size)
+      end
+    end
+
+    tokens
+  end
+end
