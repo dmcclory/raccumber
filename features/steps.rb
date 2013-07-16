fdae963f832f09d1f4c7fc9a78171b733898f85c
Dan <danmcclory@gmail.com>
2013-07-15 23:40:57 -0400

    make set the step definitions so that they return true by default.

--- features/steps.rb
+++ features/steps.rb
@@ -1,15 +1,15 @@
 Given /that I have an object with text (.*)/ do |name|
-  name
+  true
 end
 
 And /I have a second object with text (.*)/ do |name|
-  name
+  true
 end
 
 When /I combine them/ do |name|
-  name
+  true
 end
 
 Then /I should get a third object with text (.*)/ do |name|
-  name
+  true
 end
